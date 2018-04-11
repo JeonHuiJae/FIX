83389910ae2fefc31cd918c0f2569d0249023ea9
Hyejin Hong <hhj732@naver.com>
2018-04-11 18:30:34 +0900

    데이터베이스 서버에 연결했어요 ^^

--- Fix/app/src/main/java/com/example/hhj73/fix/MainActivity.java
+++ Fix/app/src/main/java/com/example/hhj73/fix/MainActivity.java
@@ -4,9 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = firebaseDatabase.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void login(View view) {
+        EditText id = (EditText) findViewById(R.id.inputID);
+        EditText pw = (EditText) findViewById(R.id.inputPW);
+
+        String strID = id.getText().toString();
+        String strPW = pw.getText().toString();
+
+        databaseReference.child("id").push().setValue(strID);
+        databaseReference.child("pw").push().setValue(strPW);
+
     }
 
     public void seniorJoin(View view) {
